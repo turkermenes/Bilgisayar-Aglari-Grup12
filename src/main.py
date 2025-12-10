3bf80e4b3ac07e19e32049737142be2729f9abde
turkermenes <turkermenes@gmail.com>
2025-12-10 18:21:42 +0300

    update: continue development

--- src/main.py
+++ src/main.py
@@ -9,12 +9,14 @@ class NetworkTopology:
         self.connection_probabability = connection_probability
         self.nodes = []
         self.links = []
+        self.graph = {}
 
     def create_nodes_from_file(self, file_path):
         nodes_file = read_file(file_path)
         for n in nodes_file:
             seperated = n.split(';')
-            node = Node(int(seperated[0]), float(seperated[1]), float(seperated[2]))
+            node = Node(int(seperated[0]), float(seperated[1]), float(seperated[2]), self)
+            node.assign_adjacencies()
             self.nodes.append(node)
 
     def set_links_from_file(self, file_path):
@@ -24,11 +26,27 @@ class NetworkTopology:
             link = Link(int(seperated[0]), int(seperated[1]), int(seperated[2]), int(seperated[3]), float(seperated[4]))
             self.links.append(link)
 
+    def create_graph(self):
+        for node in self.nodes:
+            self.graph[node.id] = node.adjacency_list
+        
+
 class Node:
-    def __init__(self, id: int, processing_delay: float, node_reliability: float):
+    def __init__(self, id: int, processing_delay: float, node_reliability: float, network_topology: NetworkTopology):
         self.id = id
         self.processing_delay = processing_delay # [0.5 ms - 2.0 ms]
         self.node_reliability = node_reliability # [0.95, 0.999]
+        self.adjacency_list = []
+        self.network_topology = network_topology
+
+    def assign_adjacencies(self):
+        for link in self.network_topology.links:
+            if link.node1_id == self.id:
+                self.adjacency_list.append(link.node2_id)
+
+            elif link.node1_id > self.id:
+                break
+
 
     def __str__(self):
         return f'id: {self.id}, processing_delay: {self.processing_delay}, node_reliability: {self.node_reliability}'
@@ -41,20 +59,38 @@ class Link:
         self.link_delay = link_delay # [3 ms, 15 ms]
         self.link_reliability = link_reliability # [0.95, 0.999]
 
+    def __str__(self):
+        return f'node1_id: {self.node1_id}, node2_id: {self.node2_id}, bandwidth: {self.bandwidth}, link_delay: {self.link_delay}, link_reliability: {self.link_reliability}'
+
 class Path:
-    def __init__(self, nodes: list, links: list):
+    def __init__(self, network_topology: NetworkTopology, nodes: list):
+        self.network_topology = network_topology
         self.nodes = nodes
-        self.links = links
+        self.links = []
+
+    def assign_links(self):
+        for link in self.network_topology.links:
+
+            for i in range(len(self.nodes) - 1):
+                j = i + 1
+                small = min(self.nodes[i].id, self.nodes[j].id)
+                large = max(self.nodes[i].id, self.nodes[j].id)
+
+                if link.node1_id == small and link.node2_id == large:
+                    self.links.append(link)
 
     def __str__(self):
         for node in self.nodes:
             print(f"{node.id} -> ", end=" ")
 
-class Metrics: 
-    def calculate_metrics(self, path: Path):
-        total_delay = self.calculate_total_delay(path)
-        total_reliability = self.calculate_total_reliability(path)
-        resource_cost = self.calculate_resource_cost(path)
+class MetricsCalculator: 
+    def __init__(self, path: Path):
+        self.path = path
+        
+    def calculate_metrics(self):
+        total_delay = self.calculate_total_delay()
+        total_reliability = self.calculate_reliability_cost()
+        resource_cost = self.calculate_resource_cost()
 
         print(f"Total Delay: {total_delay}")
         print(f"Total Reliability: {total_reliability}")
@@ -62,39 +98,48 @@ class Metrics:
 
         return total_delay, total_reliability, resource_cost
 
-    def calculate_total_delay(path: Path):
+    def calculate_total_delay(self):
         sum_link_delay = 0
         sum_processing_delay = 0
 
-        for link in path.links:
+        for link in self.path.links:
             sum_link_delay += link.link_delay
 
-        for i in range(len(path.nodes)):
-            if i != 0 and i != len(path.nodes) - 1:
-                sum_processing_delay += path.nodes[i].processing_delay
+        for i in range(len(self.path.nodes)):
+            if i != 0 and i != len(self.path.nodes) - 1:
+                sum_processing_delay += self.path.nodes[i].processing_delay
 
         return sum_link_delay + sum_processing_delay
 
-    def calculate_reliability_cost(path: Path):
+    def calculate_reliability_cost(self):
         import math
         sum_link_reliability = 0
         sum_node_reliability = 0
 
-        for link in path.links:
+        for link in self.path.links:
             sum_link_reliability += -(math.log(link.link_reliability))
 
-        for node in path.nodes:
+        for node in self.path.nodes:
             sum_node_reliability += -(math.log(node.node_reliability))
 
         return sum_link_reliability + sum_node_reliability
 
-    def calculate_resource_cost(path: Path):
+    def calculate_resource_cost(self):
         resource_cost = 0
 
-        for link in path.links:
+        for link in self.path.links:
             resource_cost += (1000 / link.bandwidth)
 
         return resource_cost
+
+# Çok Amaçlı Çözüm    
+def weighted_sum_method(path: Path, weight_delay, weight_reliability, weight_resource):
+    metrics = MetricsCalculator(path)
+    total_delay, total_reliability, resource_cost = metrics.calculate_metrics()
+
+    total_cost = weight_delay * total_delay + weight_reliability * total_reliability + weight_resource * resource_cost
+
+    return total_cost
 
 def read_file(file_path):
     result = []
@@ -107,16 +152,12 @@ def read_file(file_path):
 
     return result[1:]
 
-# Path -> P = {n1, n2, ..., nk}
-# Path = [1, 63, 24, 256]
-
 if __name__ == '__main__':
-    
     network_topology = NetworkTopology()
+    network_topology.set_links_from_file(EDGE_DATA_PATH)
     network_topology.create_nodes_from_file(NODE_DATA_PATH)
-
-    for node in network_topology.nodes:
-        print(node)
+    network_topology.create_graph()
+    print(network_topology.graph)
 
 
     # En iyi yolu bulma algoritması uygulanması (başka dosyada geliştirilecek ve burada import edilip parametreleri verilerek kullanılacaktır.)
